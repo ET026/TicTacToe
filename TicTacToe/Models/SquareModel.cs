1874d4a1a91660203814d788a3b896b929f198ec
Erenay Tozun <Erenay12345@outlook.com>
2024-02-16 17:57:22 +0100

    going away, saving my progress.

---
+++ TicTacToe/Models/SquareModel.cs
@@ -0,0 +1,28 @@
+﻿namespace TicTacToe.Models
+{
+    public class SquareModel
+    {
+        private bool isOccupied;
+
+        public int Id { get; set; }
+        public string Style { get; set; }
+
+        public bool IsOccupied
+        {
+            get => isOccupied;
+            set
+            {
+                isOccupied = value;
+                if(isOccupied)
+                {
+                    Style = "background-color: #f0f0f0; cursor: not-allowed;";
+                }
+                else
+                {
+                    Style = "background-color: #f0f0f0; cursor: pointer;";
+                }
+                
+            }
+        }
+    }
+}
---
+++ TicTacToe/Pages/BlazorTicTacToeComponent.cs
@@ -0,0 +1,23 @@
+﻿using TicTacToe.Models;
+
+namespace TicTacToe.Pages
+{
+    public partial class BlazorTicTacToeComponent
+    {
+        int score = 0;
+        string player1 = ""; 
+        string player2 = ""; 
+        string currentPlayer = "";
+        int hitPosition = 0;
+
+        public List<SquareModel> Squares { get; set; } = new List<SquareModel>();
+
+        public BlazorTicTacToeComponent()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Squares.Add(new SquareModel { Id = i });
+            }
+        }
+    }
+}
