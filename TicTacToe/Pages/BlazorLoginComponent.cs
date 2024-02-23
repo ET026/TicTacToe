5a698877365e4f3b5680537716ad7ec2c97b6cf4
Erenay Tozun <Erenay12345@outlook.com>
2024-02-23 01:36:25 +0100

    kleine updates

--- TicTacToe/Pages/BlazorLoginComponent.cs
+++ TicTacToe/Pages/BlazorLoginComponent.cs
@@ -2,5 +2,29 @@
 {
     public partial class BlazorLoginComponent
     {
+        private string player1 = "";
+        private string player2 = "";
+        private string currentPlayer = "";
+        private bool isGameRunning = true;
+
+        public string Player1
+        {
+            get => player1;
+            set
+            {
+                player1 = value;
+                
+            }
+        }
+        public string Player2
+        {
+            get => player2;
+            set
+            {
+                player2 = value;
+                
+            }
+        }
+        
     }
 }
